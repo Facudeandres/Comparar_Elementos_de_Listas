68172fb0a1341135591b0d70fbf8f656f4a3f03c
Facudeandres <115202933+Facudeandres@users.noreply.github.com>
2023-03-05 20:55:51 -0300

    Update Comparaciones.py

--- Comparaciones.py
+++ Comparaciones.py
@@ -2,44 +2,56 @@ import streamlit as st
 import pandas as pd
 import openpyxl as px
 
+# Configuración de la página de Streamlit
+st.set_page_config(page_title="Comparación de elementos de listas", page_icon=":mag:", layout="wide")
+
 # Título de la página
-st.title("Comparar elementos de dos columnas")
+st.title("Comparación de elementos de listas")
 
-# Ingreso de los títulos de las columnas
-titulo_columna_1 = st.text_input("Ingrese el título de la columna 1:")
-titulo_columna_2 = st.text_input("Ingrese el título de la columna 2:")
+# Ingreso de datos
+st.header("Ingreso de datos")
+st.write("Ingrese los títulos de las columnas y los elementos de las dos listas a comparar.")
 
-# Ingreso de las columnas de datos
-columna_1 = st.text_area("Ingrese la columna 1 (separe los elementos con salto de línea):")
-columna_2 = st.text_area("Ingrese la columna 2 (separe los elementos con salto de línea):")
+titulo_col1 = st.text_input("Título columna 1")
+titulo_col2 = st.text_input("Título columna 2")
+columna1 = st.text_area("Columna 1 (separe los elementos con salto de línea)", height=250)
+columna2 = st.text_area("Columna 2 (separe los elementos con salto de línea)", height=250)
 
-# Dividir las cadenas de entrada en listas
-lista_1 = [x.strip() for x in columna_1.split('\n') if x.strip()]
-lista_2 = [x.strip() for x in columna_2.split('\n') if x.strip()]
+# Conversión de las listas ingresadas a DataFrames de Pandas
+df1 = pd.DataFrame(columna1.split("\n"), columns=[titulo_col1])
+df2 = pd.DataFrame(columna2.split("\n"), columns=[titulo_col2])
 
-# Comparación de las listas
-solo_en_a = sorted(set(lista_1) - set(lista_2))
-solo_en_b = sorted(set(lista_2) - set(lista_1))
-coincidencias = sorted(set(lista_1) & set(lista_2))
+# Comparación de los elementos de las listas
+coincidencias = []
+for index1, row1 in df1.iterrows():
+    for index2, row2 in df2.iterrows():
+        if row1[titulo_col1] == row2[titulo_col2]:
+            coincidencias.append(row1[titulo_col1])
 
-# Visualización de los resultados
-st.write("Resultado:")
+# Creación del DataFrame de resultado
 df_resultado = pd.DataFrame({
-    f"Solo en {titulo_columna_1}": solo_en_a,
-    f"Solo en {titulo_columna_2}": solo_en_b,
+    f"Solo en {titulo_col1}": df1.loc[~df1[titulo_col1].isin(df2[titulo_col2]), titulo_col1],
+    f"Solo en {titulo_col2}": df2.loc[~df2[titulo_col2].isin(df1[titulo_col1]), titulo_col2],
     "Coincidencias": coincidencias
 })
-st.write(df_resultado)
 
-# Descargar en excel
-output = pd.ExcelWriter('comparacion_columnas.xlsx')
-df_input = pd.DataFrame({
-    titulo_columna_1: lista_1,
-    titulo_columna_2: lista_2
-})
-df_input.to_excel(output, sheet_name='Input', index=False)
-df_resultado.to_excel(output, sheet_name='Output', index=False)
-output.save()
-button = '<a href="comparacion_columnas.xlsx" download="comparacion_columnas.xlsx">Descargar resultado en Excel</a>'
-st.markdown(button, unsafe_allow_html=True)
+# Exportación del resultado a Excel
+output_excel = px.Workbook()
+hoja_input = output_excel.create_sheet("Input", 0)
+hoja_output = output_excel.create_sheet("Output", 1)
 
+for row_index, row_value in pd.concat([df1, df2], axis=1).iterrows():
+    hoja_input.append([row_value[col] for col in [titulo_col1, titulo_col2]])
+
+hoja_output.append([f"Solo en {titulo_col1}", f"Solo en {titulo_col2}", "Coincidencias"])
+for row_index, row_value in df_resultado.iterrows():
+    hoja_output.append([row_value[col] for col in df_resultado.columns])
+
+# Descarga del archivo generado
+def download_excel_file(df, filename):
+    with open(filename, 'wb') as f:
+        f.write(df)
+    return f
+
+excel_file = download_excel_file(px.writer.excel.save_virtual_workbook(output_excel), f"{titulo_col1}_{titulo_col2}.xlsx")
+st.download_button(label="Descargar resultado", data=excel_file.getvalue(), file_name=f"{titulo_col1}_{titulo_col2}.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
