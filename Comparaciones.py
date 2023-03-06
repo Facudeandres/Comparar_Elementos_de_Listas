b35b198133f245a9a4860f89ab11cd62cbee0afe
Facudeandres <115202933+Facudeandres@users.noreply.github.com>
2023-03-05 22:29:54 -0300

    Update Comparaciones.py

--- Comparaciones.py
+++ Comparaciones.py
@@ -1,26 +1,24 @@
-import  streamlit as st
+import streamlit as st
 import pandas as pd
 from dataclasses import dataclass
-import openpyxl
 
-columna1_header = st.text_input ("Ingrese el nombre de la columna 1")
-columna1_data = st.text_area ("Ingrese los datos de la columna 1")
-columna2_header = st.text_input ("Ingrese el nombre de la columna 2")
-columna2_data = st.text_area ("Ingrese los datos de la columna 2")
+columna1_header = st.text_input("Ingrese el nombre de la columna 1")
+columna1_data = st.text_area("Ingrese los datos de la columna 1")
+columna2_header = st.text_input("Ingrese el nombre de la columna 2")
+columna2_data = st.text_area("Ingrese los datos de la columna 2")
 
-columna1_lista= columna1_data.split(",")
-columna2_lista= columna2_data.split(",")
-df = pd.dataframe({columna1_header:columna1_lista, columna2_header: columna2_lista})
+columna1_lista = columna1_data.split(",")
+columna2_lista = columna2_data.split(",")
+df = pd.DataFrame({columna1_header: columna1_lista, columna2_header: columna2_lista})
 
 @dataclass
-
-class output:
+class Output:
     solo_col1: list
     solo_col2: list
     coincidencias: list
 
-def comparar_columnas (columna1,columna2):
-    output - output ([], [], [])
+def comparar_columnas(columna1, columna2):
+    output = Output([], [], [])
     for elemento in columna1:
         if elemento not in columna2:
             output.solo_col1.append(elemento)
@@ -29,14 +27,14 @@ def comparar_columnas (columna1,columna2):
             output.solo_col2.append(elemento)
         else:
             output.coincidencias.append(elemento)
-            return output
+    return output
 
 output = comparar_columnas(df[columna1_header], df[columna2_header])
-st.write((" Solo en" + columna1_header),output.solo_col1)
-st.write((" Solo en" + columna2_header),output.solo_col2)
-st.write( "Coincidencias" , output.coincidencias )
+st.write("Solo en " + columna1_header, output.solo_col1)
+st.write("Solo en " + columna2_header, output.solo_col2)
+st.write("Coincidencias", output.coincidencias)
 
 with pd.ExcelWriter('output_comparacion.xlsx') as writer:
-    pd.DataFrame({("solo en " + columna1_header ): output.solo_col1}).to_excel(writer, sheet_name='output_comparacion', index=False, startcol=0)
-    pd.DataFrame({("solo en " + columna2_header ): output.solo_col2}).to_excel(writer, sheet_name='output_comparacion', index=False, startcol=1)
+    pd.DataFrame({"solo en " + columna1_header: output.solo_col1}).to_excel(writer, sheet_name='output_comparacion', index=False, startcol=0)
+    pd.DataFrame({"solo en " + columna2_header: output.solo_col2}).to_excel(writer, sheet_name='output_comparacion', index=False, startcol=1)
     pd.DataFrame({"Coincidencias": output.coincidencias}).to_excel(writer, sheet_name='output_comparacion', index=False, startcol=2)
